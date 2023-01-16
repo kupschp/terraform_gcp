34030b26bbe63f916420e2cc6083c7ba79d408c4
Philip Kupsch <pkupsch@wayfair.com>
2023-01-16 10:05:08 +0100

    added one vm(gcp compute engine) and attached to the same network created previously, added terraform* files git ignore

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
+      source  = "hashicorp/google"
+      version = "4.48.0"
     }
   }
 }
@@ -17,4 +17,21 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
